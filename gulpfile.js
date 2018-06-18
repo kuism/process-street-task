7e1d73b36fd7a3260e93544c2142072abc73e8ff
Nijesh KU <nijeshku1992@gmail.com>
2018-06-18 13:52:19 +0530

    test cases removed'

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,7 @@
 
 var gulp = require('gulp');
 var wrench = require('wrench');
-var connect = require('gulp-connect'),
-	karma = require('karma').server,
-    protractor = require("gulp-protractor").protractor;
+
 /**
  *  This will load all js or coffee files in the gulp directory
  *  in order to load all gulp tasks
@@ -24,20 +22,3 @@ gulp.task('default', ['clean'], function () {
 	gulp.start('build');
 });
 
-
-gulp.task('unit', function (done) {
-    karma.start({
-        configFile: __dirname + '/tests/karma.conf.js',
-        singleRun: true
-    }, done);
-});
-
-gulp.task('e2e', function(done) {
-    var args = ['--baseUrl', 'http://127.0.0.1:8888'];
-    gulp.src(["./tests/e2e/*.js"])
-        .pipe(protractor({
-            configFile: "tests/protractor.conf.js",
-            args: args
-        }))
-        .on('error', function(e) { throw e; });
-});
--- tests/e2e/spec.js
+++
@@ -1,4 +0,0 @@
-describe('UploadPageCtrl', function () {
-
-
-});
